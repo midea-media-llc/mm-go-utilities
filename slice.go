c60576c2b27234798356211ea0b1eb6d60df944b
lunk <nguyenkhoalu97@gmail.com>
2023-02-17 10:33:50 +0700

    Update slice.go

--- slice.go
+++ slice.go
@@ -33,3 +33,67 @@ func StringFormat[T any](source string, params ...T) string {
 
 	return source
 }
+
+func Select[T any, V any](sources []T, predicate func(T) V) []V {
+	result := make([]V, len(sources))
+	for i, e := range sources {
+		result[i] = predicate(e)
+	}
+
+	return result
+}
+
+func Where[T any](sources []T, conditional func(T) bool) []T {
+	result := make([]T, 0)
+	for _, e := range sources {
+		if conditional(e) {
+			result = append(result, e)
+		}
+	}
+
+	return result
+}
+
+func Find[T any](sources []T, predicate func(T) bool) T {
+	for _, e := range sources {
+		if predicate(e) {
+			return e
+		}
+	}
+
+	isPointer := false
+	typeT := reflect.TypeOf(sources).Elem()
+	for ComparableContains(typeT.Kind(), reflect.Pointer) {
+		typeT = typeT.Elem()
+		isPointer = true
+	}
+
+	v := reflect.New(typeT).Interface()
+	if isPointer {
+		v = &v
+	}
+	return v.(T)
+}
+
+func FindLast[T any](sources []T, predicate func(T) bool) T {
+	for i := len(sources) - 1; i >= 0; i++ {
+		e := sources[i]
+		if predicate(e) {
+			return e
+		}
+
+	}
+
+	isPointer := false
+	typeT := reflect.TypeOf(sources).Elem()
+	for ComparableContains(typeT.Kind(), reflect.Pointer) {
+		typeT = typeT.Elem()
+		isPointer = true
+	}
+
+	v := reflect.New(typeT).Interface()
+	if isPointer {
+		v = &v
+	}
+	return v.(T)
+}
