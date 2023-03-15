0699597c1ceba589dd658518fd2bdb6c05fff220
lunk <nguyenkhoalu97@gmail.com>
2023-03-15 13:49:44 +0700

    Update name.go

--- name.go
+++ name.go
@@ -1,23 +1,56 @@
 package utils
 
+type INameItem[T comparable] interface {
+	GetId() T
+	GetName() *string
+}
+
+type NameItem[T any] struct {
+	Id   T
+	Name *string
+}
+
+func (e *NameItem[T]) GetId() T {
+	return e.Id
+}
+
+func (e *NameItem[T]) GetName() *string {
+	return e.Name
+}
+
 type IName[T comparable] interface {
-	InternalFindItemName(id T, claims IClaims) *string
+	InternalFindItemName(id T, claims IClaims) INameItem[T]
+}
+
+type INames[T comparable] interface {
+	InternalFindItemNameByListID(ids []*T, claims IClaims) []INameItem[T]
 }
 
 type FindNameModel[T comparable] struct {
-	Id          *T
-	Module      IName[T]
-	OnCompleted func(*string)
+	Id           *T
+	Id2          []*T
+	Module       IName[T]
+	Module2      INames[T]
+	OnCompleted  func(INameItem[T])
+	OnCompleted2 func([]INameItem[T])
 }
 
 func FindNames[T comparable](claims IClaims, models ...*FindNameModel[T]) {
 	callBacks := Select(models, func(model *FindNameModel[T]) Function {
 		return func() {
-			if model == nil || model.Id == nil {
+			if model == nil {
 				return
 			}
 
-			model.OnCompleted(model.Module.InternalFindItemName(*model.Id, claims))
+			if model.Id != nil {
+				result := model.Module.InternalFindItemName(*model.Id, claims)
+				model.OnCompleted(result)
+			}
+
+			if model.Id2 != nil {
+				results := model.Module2.InternalFindItemNameByListID(model.Id2, claims)
+				model.OnCompleted2(results)
+			}
 		}
 	})
 
