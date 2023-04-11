5751df42752d0569708f8d5974d722317bbd2c29
lunk <nguyenkhoalu97@gmail.com>
2023-04-11 22:16:21 +0700

    Update errors.go

--- errors.go
+++ errors.go
@@ -2,8 +2,10 @@ package utils
 
 import (
 	"errors"
+	"net/http"
 	"reflect"
 
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
@@ -26,7 +28,45 @@ func HandleSqlError(err error) error {
 // If the provided error is not a gRPC error, the function simply returns the original error.
 func HandleGrpcError(err error) error {
 	if e, ok := status.FromError(err); ok {
+		if e.Code() == codes.InvalidArgument {
+			return NewValidateHttpError(err)
+		}
 		return errors.New(e.Message())
 	}
 	return err
+}
+
+func NewValidateHttpError(err error) error {
+	if e, ok := status.FromError(err); ok {
+		if e.Code() == codes.InvalidArgument {
+			return &validateError{err: "888", field: e.Message(), status: http.StatusBadRequest}
+		}
+		return errors.New(e.Message())
+	}
+
+	return err
+}
+
+type IValidateError interface {
+	Error() string
+	Status() int
+	Field() string
+}
+
+type validateError struct {
+	err    string
+	field  string
+	status int
+}
+
+func (s *validateError) Error() string {
+	return s.err
+}
+
+func (s *validateError) Field() string {
+	return s.field
+}
+
+func (s *validateError) Status() int {
+	return s.status
 }
